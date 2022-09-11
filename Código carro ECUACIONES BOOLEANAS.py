6cab9b47062423b8f467bead48b33e206811c1c5
CaMiLo TuTa <camilotuta2004@gmail.com>
2022-09-11 15:30:13 -0500

    cuarto cambio

--- Código carro ECUACIONES BOOLEANAS.py
+++ Código carro ECUACIONES BOOLEANAS.py
@@ -1,25 +1,26 @@
 
-#*  LLAMADA A LAS LIBRERÍAS NECESARIAS
+# *  LLAMADA A LAS LIBRERÍAS NECESARIAS
 from time import sleep, sleep_ms
 from machine import Pin as pin
 
-#*  PUERTAS
+# *  PUERTAS
 capó = pin(5, pin.IN, pin.PULL_UP)
 puerta_derecha = pin(18, pin.IN, pin.PULL_UP)
 puerta_izquierda = pin(19, pin.IN, pin.PULL_UP)
 baúl = pin(21, pin.IN, pin.PULL_UP)
-#*  LED/S
+# *  LED/S
 led = pin(22, pin.OUT)
 
 
-#?  CICLO PRINCIPAL
+# ?  CICLO PRINCIPAL
 while True:
-    #*   VARIABLE ENCARGADA DE ALMACENAR EL VALOR DE CADA PUERTA, Y DONDE EL "or" SUMA TODOS LOS VALORES
-    encendido = capó.value() or  puerta_derecha.value() or  puerta_izquierda.value() or baúl.value()
 
-    #*   IMPRESIÓN DE LOS VALORES CORRESPONDIENTES A LOS PULSADORES DE LAS PUERTAS (OPCIONAL)
-    print(capó.value(),  puerta_derecha.value(),  puerta_izquierda.value(), baúl.value())
+    # *   VARIABLE ENCARGADA DE ALMACENAR EL VALOR DE CADA PUERTA, Y DONDE EL "or" SUMA TODOS LOS VALORES
+    encendido = capó.value() or puerta_derecha.value() or puerta_izquierda.value() or baúl.value()
+
+    # *   IMPRESIÓN DE LOS VALORES CORRESPONDIENTES A LOS PULSADORES DE LAS PUERTAS (OPCIONAL)
+    print(capó.value(),  puerta_derecha.value(), puerta_izquierda.value(), baúl.value())
     sleep_ms(50)
 
-    #*   SE ASIGNA LA SUMA DE LOS VALORES QUE TIENEN LAS PUERTAS AL LED
+    # *   SE ASIGNA LA SUMA DE LOS VALORES QUE TIENEN LAS PUERTAS AL LED
     led.value(encendido)
